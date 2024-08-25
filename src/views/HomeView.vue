ab0c9bff965dc149275e253e8dbb70d101e10ebe
RomanLytvyn <romanlytvynua@gmail.com>
2024-08-25 12:36:48 +0300

    Fix home page title spelling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="container-fluid text-center">
     <div class="mb-5">
-      <h1>Histify</h1>
+      <h1>Hostify</h1>
       <figcaption class="blockquote-footer" style="margin-top: -5px !important; color: lightgray;">
         Your professional mafia game host assistant
       </figcaption>
