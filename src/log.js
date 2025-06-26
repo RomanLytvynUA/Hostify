6810103da322d98991d698eb14b82a5b6022616b
Roman Lytvyn <romanlytvynua@gmail.com>
2025-06-26 12:10:47 +0300

    add a global functions for debugging log

--- src/log.js
+++ src/log.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 
 export const useGameLog = defineStore('log', () => {
     const log = ref("")
-    
+
     const generateLogHeader = (tournamentName, game, stage, date) => {
         log.value = "---[INFO]---\n"
         log.value += `| Tournament: ${tournamentName}\n`
@@ -11,7 +11,7 @@ export const useGameLog = defineStore('log', () => {
         log.value += `| Stage: ${stage}\n`
         log.value += `| Date: ${date}`
     }
-    
+
     const generatePlayersList = (playersData) => {
         log.value += "\n\n"
         log.value += "---[PLAYERS]---\n"
@@ -26,23 +26,32 @@ export const useGameLog = defineStore('log', () => {
     }
 
     const logEvent = (event) => {
-        const now = new Date(); const hours = now.getHours().toString().padStart(2, '0'); 
-        const minutes = now.getMinutes().toString().padStart(2, '0'); 
+        const now = new Date();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
 
         log.value += `| [${hours}:${minutes}] ${event}\n`
     }
 
     const downloadGameLog = () => {
         const blob = new Blob([log.value], { type: 'text/plain' });
-        
+
         const link = document.createElement('a');
         link.download = 'gameLog.txt';
         link.href = window.URL.createObjectURL(blob);
-        
+
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
     }
 
-    return { downloadGameLog, generateLogHeader, generatePlayersList, logPhase, logEvent }
+    return { downloadGameLog, generateLogHeader, generatePlayersList, logPhase, logEvent, log }
 })
+
+window.getLog = function () {
+    console.log(useGameLog().log);
+};
+
+window.dlLog = function () {
+    console.log(useGameLog().downloadGameLog());
+};
