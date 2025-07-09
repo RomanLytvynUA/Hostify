f1465ea1288d25a9a21180d89015619e0bb10009
Roman Lytvyn <romanlytvynua@gmail.com>
2025-07-09 12:59:57 +0300

    fix navbar elements alignment

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -17,11 +17,11 @@ const setLocale = (localeName) => {
 
 
 <template>
-    <nav style="background-color: #333334 !important" class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav style="background-color: #333334 !important;" class="navbar navbar-expand-lg">
         <div class="container-fluid position-relative">
 
             <!-- Centered section -->
-            <div class="justify-content-center d-flex align-items-center" style="flex-grow: 1;">
+            <div class="d-flex justify-content-center align-items-center w-100">
                 <RouterLink style="color: white; margin: 0;" class="navbar-brand d-flex align-items-center" to="/">
                     <img src="../assets/logo.png" alt="logo" width="50" style="margin-right: 5px;">
                     {{ routeName == 'home' ? '' : 'Hostify' }}
@@ -31,8 +31,9 @@ const setLocale = (localeName) => {
             </div>
 
             <!-- Right-aligned section -->
-            <ul class="navbar-nav ms-auto d-flex align-items-center">
-                <li class="nav-item dropdown" style="padding-right: 16px;">
+            <ul class="navbar-nav d-flex align-items-center position-absolute"
+                style="right: 16px; top: 50%; transform: translateY(-50%);">
+                <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         {{ t('localeEmoji') }}
